5da0a8d8a792d18cd83d52ab30c9e6ff4a717a97
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2023-12-25 05:10:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ const app = express();
 
 const middlewares = [
   cors({
-    origin: ["https://buildyourownwebsite.netlify.app", "http://localhost:3000"],
+    origin: ["https://buildyourownwebsite.netlify.app",
+      "https://loveletter.netlify.app/",
+      "http://localhost:3000"],
     credentials: true
   }),
   express.json(),
